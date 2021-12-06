6e6b0ce12fdc69ba54e41f0b80ebe2bbeda94189
Franklyn <franklyn1998@live.com>
2021-12-06 11:01:21 -0300

    findAll implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -6,11 +6,16 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
-	
+
 	void insert(Seller obj);
+
 	void update(Seller obj);
+
 	void deleteById(Integer id);
+
 	Seller findById(Integer id);
+
 	List<Seller> findAll();
+
 	List<Seller> findByDepartment(Department department);
 }
